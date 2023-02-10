4584026e17cd370381ea3f92ceabea6d06fc5902
testinguser700 <77050798+testinguser700@users.noreply.github.com>
2023-02-10 13:41:42 +0530

    Update TL2.js

--- TL2.js
+++ TL2.js
@@ -4,3 +4,10 @@ var x = 10;
 var y = 20;  
 console.log(`The product of the variables ${x} and ${y} is:  
  ${x*y}`);  
+
+//Simple Interest
+
+var x = 10;  
+var y = 20;  
+console.log(`The product of the variables ${x} and ${y} is:  
+ ${x*y}`);  
