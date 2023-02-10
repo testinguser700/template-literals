ee4c425a8e4190b04a42bf83d36260876f79e5c8
testinguser700 <77050798+testinguser700@users.noreply.github.com>
2023-02-10 13:30:14 +0530

    Create TL1.js

---
+++ TL1.js
@@ -0,0 +1,6 @@
+// Without template literal   
+console.log('Without template literal \n multiline string');   
+    
+// With template literal   
+console.log(`Using template literal  
+multiline string`);  
